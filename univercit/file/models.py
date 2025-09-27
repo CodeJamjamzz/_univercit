ad17943337bc3b59253acda4f2baf4500baea7a7
SuperaNova <jared.acebes@gmail.com>
2025-09-27 21:54:32 +0800

    model of testimonials and file

---
+++ univercit/file/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class File(models.Model):
+    file_id = models.AutoField(primary_key=True)
+    file_url = models.FileField(upload_to='files/')
+    file_desc = models.TextField()
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    upvote_count = models.IntegerField(default=0)
+    downvote_count = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.file_name
--- univercit/univercit/settings.py
+++ univercit/univercit/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testimonial',
+    'file'
 ]
 
 MIDDLEWARE = [
@@ -73,19 +75,19 @@ WSGI_APPLICATION = 'univercit.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'univercit',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'OPTIONS': {'init_command': "SET SQL_MODE='STRICT_TRANS_TABLES'"},
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'univercit',
+        'USER': 'root',
+        'PASSWORD': '$up3rN0v@',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET SQL_MODE='STRICT_TRANS_TABLES'"},
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
