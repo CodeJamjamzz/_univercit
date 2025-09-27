bcfc798a7a4955b1bdcc8e965bd477714f2c7230
Carl <carlpepino902@gmail.com>
2025-09-27 22:39:34 +0800

    added user app

--- univercit/curriculum/models.py
+++ univercit/curriculum/models.py
@@ -6,8 +6,14 @@ class Program(models.Model):
     program_desc = models.TextField()
     courses = models.ManyToManyField('Course', related_name="programs")
 
+    def __str__(self):
+        return self.program_code
+
 
 class Course(models.Model):
     course_id = models.CharField(max_length=10, primary_key=True)
     course_name = models.CharField(max_length=50)
     course_desc = models.TextField()
+
+    def __str__(self):
+        return self.course_name
