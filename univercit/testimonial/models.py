ca4aab9147baef998c49930bb97224df1ee78c99
SuperaNova <jared.acebes@gmail.com>
2025-09-27 23:39:46 +0800

    fix

--- univercit/testimonial/models.py
+++ univercit/testimonial/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Testimonial(models.Model):
     testimonial_id = models.AutoField(primary_key=True)
+    course_id = models.ForeignKey('curriculum.Course', on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
     is_visible = models.BooleanField(default=True)
 
