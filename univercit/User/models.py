20afacbf8b50aa781ee99c41e38715df3c580c7d
Jamjamzz <codejamzz27@gmail.com>
2025-09-27 22:38:40 +0800

    User Model

---
+++ univercit/User/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+# Create your models here.fdsfsdfa
+class Student(models.Model):
+    studentId = models.AutoField(max_length=50, primary_key=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"""id: {self.student_id}
+        Firstname: {self.fname}
+        Lastname: {self.lname}
+        Username: {self.username}
+        Email: {self.email}
+        """
+
