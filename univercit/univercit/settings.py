82b9e81006f846f130007624290a8f15903bbe3d
jermochi <jrvnryle@gmail.com>
2025-10-19 17:16:40 +0800

    did login page UI

--- univercit/univercit/settings.py
+++ univercit/univercit/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'univercit',
         'USER': 'root',
-        'PASSWORD': 'jamjam27',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET SQL_MODE='STRICT_TRANS_TABLES'"},
