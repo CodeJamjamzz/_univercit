f156ed4b052bf8afa17df9626cda720f3b0ac8b3
Jamjamzz <codejamzz27@gmail.com>
2025-11-26 09:00:44 +0800

    user ui

--- univercit/curriculum/urls.py
+++ univercit/curriculum/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 # Create your tests here.
-urlpatterns = [
-    path("", views.index, name="index")
-]
+# urlpatterns = [
+#     path("", views.index, name="index")
+# ]
--- univercit/user/views.py
+++ univercit/user/views.py
@@ -1,44 +1,49 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
-from curriculum.models import Program
 from .models import Student
+from curriculum.models import Program
 
 # Create your views here.
-def student_list(request):
-    students = Student.objects.all()
-    return render(request, 'student_list.html', {'students': students})
+def index(request):
+    return render(request, 'index.html')
 
 def student_list(request):
-    students = Student.objects.all()
-    return render(request, 'student_list.html', {'students': students})
+    students = list(Student.objects.values())
+    return JsonResponse({'students': students})
 
 def student_detail(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
-    return render(request, 'student_detail.html', {'student': student})
+    return JsonResponse({
+        'studentId': student.studentId,
+        'programId': student.programId_id,
+        'fname': student.fname,
+        'lname': student.lname,
+        'username': student.username,
+        'email': student.email,
+        'password': student.password
+    })
 
 def student_edit(request, student_id):
-    student = get_object_or_404(Student, pk=student_id)
-
     if request.method == 'POST':
+        student = get_object_or_404(Student, pk=student_id)
         program_id = request.POST.get('programId')
-        student.programId = Program.objects.get(pk=program_id) if program_id else None
+        student.programId_id = program_id if program_id else None
         student.fname = request.POST.get('fname')
         student.lname = request.POST.get('lname')
         student.username = request.POST.get('username')
         student.email = request.POST.get('email')
         student.password = request.POST.get('password')
         student.save()
-        return redirect('student_list')
-
-    programs = Program.objects.all()
-    return render(request, 'student_form.html', {'student': student, 'programs': programs})
+        return HttpResponse("Student updated successfully.")
+    return HttpResponse("Use POST method to edit student.")
 
 def student_delete(request, student_id):
-    student = get_object_or_404(Student, pk=student_id)
     if request.method == 'POST':
+        student = get_object_or_404(Student, pk=student_id)
         student.delete()
-        return redirect('student_list')
-    return render(request, 'student_confirm_delete.html', {'student': student})
+        return HttpResponse("Student deleted successfully.")
+    return HttpResponse("Use POST method to delete student.")
 
 
 class LogInView(View):
@@ -58,18 +63,20 @@ class LogInView(View):
 
 class SignInView(View):
     template = "signin.html"
-    def get(self, request):
-        return render(request, self.template)
-    def post(self, request):
-        program_code = request.POST['program']
-        program = Program.objects.get(program_code=program_code)
 
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirmpassword = request.POST['confirm_password']
+    def get(self, request):
+        programs = Program.objects.all()
+        return render(request, self.template, {'programs': programs})
+    def post(self, request):
+        program_code = request.POST.get("programId")
+        program = Program.objects.get(program_code=program_code)
+        firstname = request.POST.get('fname')
+        lastname = request.POST.get('lname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirmpassword = request.POST.get('confirm_password')
+
         if confirmpassword != password:
             return render(request, self.template, {'error': 'confirm password incorrect'})
 
@@ -81,10 +88,6 @@ class SignInView(View):
             email=email,
             password=password
         )
+
         return redirect('index')
 
-class HomeView(View):
-
-    template = "index.html"
-    def get(self, request):
-        return render(request, self.template)
