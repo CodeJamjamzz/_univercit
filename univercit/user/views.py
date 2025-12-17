a3a1f56446b909dd5d1bc08d7e30b3c82b2101ee
Jamjamzz <codejamzz27@gmail.com>
2025-12-17 15:40:25 +0800

    programId fix

--- univercit/user/views.py
+++ univercit/user/views.py
@@ -8,16 +8,113 @@ from django.views import View
 from .models import Student
 from django.db import connection
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def get_dashboardstatsrow():
+    with connection.cursor() as cursor:
+        cursor.callproc(
+            'dashboard_getstatsrow',
+            [0, 0, 0, 0, 0]
+        )
+
+        # Fetch ALL OUT parameters
+        cursor.execute("""
+            SELECT
+                @_dashboard_getstatsrow_0,  -- total_students
+                @_dashboard_getstatsrow_1,  -- total_programs
+                @_dashboard_getstatsrow_2,  -- total_courses
+                @_dashboard_getstatsrow_3,  -- total_files
+                @_dashboard_getstatsrow_4;  -- status
+        """)
+
+        (
+            total_students,
+            total_programs,
+            total_courses,
+            total_files,
+            status
+        ) = cursor.fetchone()
+
+    return {
+        "total_students": total_students,
+        "total_programs": total_programs,
+        "total_courses": total_courses,
+        "total_files": total_files,
+        "status": status
+    }
+
+
+def dashboard_view():
+    with connection.cursor() as cursor:
+        cursor.callproc('chart_studentperprogram')
+        rows = cursor.fetchall()
+
+    total_students = sum(row[1] for row in rows) or 1
+
+    program_stats = [
+        {
+            "program_name": row[0],
+            "student_count": row[1],
+            "student_pct": round((row[1] / total_students) * 100, 1)
+        }
+        for row in rows
+    ]
+
+    return program_stats
+
+def get_courses_per_program():
+    with connection.cursor() as cursor:
+        cursor.callproc('chart_courseperprogram')
+        rows = cursor.fetchall()  # list of tuples: (program_code, program_name, course_count)
+
+    total_courses = sum(row[2] for row in rows) or 1
+
+    program_stats = [
+        {
+            "program_code": row[0],
+            "program_name": row[1],
+            "course_count": row[2],
+            "course_pct": round((row[2] / total_courses) * 100, 1)
+        }
+        for row in rows
+    ]
+
+    return program_stats
+
 class DashboardView(View):
     template = 'dashboard.html'
+
     def get(self, request):
-        return render(request, self.template)
+        data = get_dashboardstatsrow()
+        total_students = data["total_students"]
+        total_programs = data["total_programs"]
+        total_courses = data["total_courses"]
+        total_files = data["total_files"]
+        status = int(data["status"])
+
+        students_per_program = dashboard_view()
+        course_per_program = get_courses_per_program()
+
+        if status == 1:
+            print("SQLExcetpion occured")
+            return render(request, self.template)
+
+        return render(request, self.template,
+                      {"total_students": total_students,
+                       "total_programs": total_programs,
+                       "total_courses": total_courses,
+                       "total_files": total_files,
+                       "program_stats": students_per_program,
+                       "courseperprogram_stats": course_per_program})
+
 
 class LogInView(View):
     template = "login.html"
+
     def get(self, request):
         return render(request, self.template)
 
@@ -42,6 +139,7 @@ class LogInView(View):
             print("DID NOT LOGIN")
             return render(request, "login.html", {"error": "Invalid credentials"})
 
+
 class SignInView(View):
     template = "signin.html"
 
@@ -62,9 +160,11 @@ class SignInView(View):
         # if not programs:
         #     programs = [{"program_code": "None", "program_desc": "No programs available"}]
         # return render(request, self.template, {'programs': programs})
+
     def post(self, request):
         programs = self.get_programs()
-        program_code = request.POST.get("program_code")
+        program_code = request.POST.get("programId")
+        print(program_code)
         firstname = request.POST.get('fname')
         lastname = request.POST.get('lname')
         username = request.POST.get('username')
@@ -75,13 +175,17 @@ class SignInView(View):
         if password != confirm_password:
             return render(request, self.template, {"error": "Passwords do not match.", 'programs': programs})
         if len(password) < 8:
-            return render(request, self.template, {"error": "Password must be at least 8 characters long.", 'programs': programs})
+            return render(request, self.template,
+                          {"error": "Password must be at least 8 characters long.", 'programs': programs})
         if not re.search(r"[A-Z]", password):
-            return render(request, self.template,{"error": "Password must contain at least one uppercase letter.", 'programs': programs})
+            return render(request, self.template,
+                          {"error": "Password must contain at least one uppercase letter.", 'programs': programs})
         if not re.search(r"[a-z]", password):
-            return render(request, self.template,{"error": "Password must contain at least one lowercase letter.", 'programs': programs})
+            return render(request, self.template,
+                          {"error": "Password must contain at least one lowercase letter.", 'programs': programs})
         if not re.search(r"[0-9]", password):
-            return render(request, self.template,{"error": "Password must contain at least one number.", 'programs': programs})
+            return render(request, self.template,
+                          {"error": "Password must contain at least one number.", 'programs': programs})
 
         with connection.cursor() as cursor:
             cursor.callproc("user_signup", [
@@ -105,4 +209,3 @@ class SignInView(View):
                 return redirect('/curriculum/programs/')
             elif status == 3:
                 return render(request, self.template, {"error": "Passwords does not match.", 'programs': programs})
-
