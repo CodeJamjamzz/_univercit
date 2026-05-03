ba27d79db557b2d404a00b64bc030ec2721c32be
Jamjamzz <code.jamjamzz@gmail.com>
2026-05-03 17:12:19 +0800

    registration & admin

--- univercit/user/urls.py
+++ univercit/user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import delete_student
 
 urlpatterns = [
     path('signin/', views.SignInView.as_view(), name="signin"),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name="student_logout"),
     path('', views.index, name="index"),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('dashboard/updateform/<int:id>/', views.TableView.as_view(), name='update_student'),
+    path('dashboard/delete/<int:id>/', delete_student, name='delete_student'),
 ]
